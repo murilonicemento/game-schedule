59b855d6a9959f1e228d86a31ad2cf5740b11b30
murilonicemento <murilo_nascimento22@outlook.com>
2022-11-03 14:23:00 -0300

    Personalizando e incluindo animações ao projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function createGame( player1, hour, player2){
+function createGame(player1, hour, player2) {
     return `
     <li>
         <img src="assets/img/icon-${player1}.svg" alt="Bandeira do ${player2}" />
@@ -7,10 +7,11 @@ function createGame( player1, hour, player2){
     </li>
 `
 }
-
-function createCard(date, day, games){
+let delay = -.3
+function createCard(date, day, games) {
+    delay += .8
     return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
            ${games}
@@ -18,14 +19,7 @@ function createCard(date, day, games){
     </div>
 `
 }
-document.querySelector("#app").innerHTML =
-`<header>
-    <img src="assets/img/logo.svg" alt="Logo da NLW">
-</header>
-<main id="cards">
-${createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameron") +  createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))}
-${createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland"))}
-${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameron"))}
+document.querySelector("#cards").innerHTML =
+    createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameron") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia")) +
+    createCard("28/11", "segunda", createGame("south-korea", "10:00", "ghana") + createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguai"))
 
-</main>
-`
