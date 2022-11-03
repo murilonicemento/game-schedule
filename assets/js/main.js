73ce36bd770cb22a65aac9729a61775912847dfd
murilonicemento <murilo_nascimento22@outlook.com>
2022-11-03 19:21:02 -0300

    Inclusão de modificações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function createGame( player1, hour, player2){
+function createGame(player1, hour, player2) {
     return `
     <li>
         <img src="assets/img/icon-${player1}.svg" alt="Bandeira do ${player2}" />
@@ -8,7 +8,7 @@ function createGame( player1, hour, player2){
 `
 }
 
-function createCard(date, day, games){
+function createCard(date, day, games) {
     return `
     <div class="card">
         <h2>${date} <span>${day}</span></h2>
@@ -19,11 +19,11 @@ function createCard(date, day, games){
 `
 }
 document.querySelector("#app").innerHTML =
-`<header>
+    `<header>
     <img src="assets/img/logo.svg" alt="Logo da NLW">
 </header>
 <main id="cards">
-${createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameron") +  createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))}
+${createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameron") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))}
 ${createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland"))}
 ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameron"))}
 
